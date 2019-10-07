2052a8a4ad2d89ae1e397c9b9a1e7e43a73d4612
uboreas <gokhan@clxdev.net>
2019-10-08 00:42:02 +0300

    initial

---
+++ README.md
@@ -0,0 +1,189 @@
+# centos-8-minimal
+
+A pure bash script to create **minimal installation ISO** image together with **additional packages**, leveraging one of official CentOS 8 ISO distribution.
+
+This will create an ISO image in the order of workflow shown below:
+
+1. Rerefence ISO will be mounted (to "mtemp/" in working folder).
+- An ISO template will be created (to "image/" in working folder).
+    - Essentials (isolinux, EFI, boot images etc) will be copied from references ISO as it is.
+    - Template files will be re-constructed and (re)placed on ISO template.
+- Dependencies for "core" and "additional" packages will be scanned and a resulting package list will be created.
+    - Required packages (RPM files) will be downloaded in this phase if it is not downloaded yet and will be added into ISO template.
+    - A copy of newly downloaded RPM file will be added into "rpms/" in working folder for later use to avoid re-downloading.
+- A repository (and metadata information) will be created under ISO template to use packages added.
+- ISO image will be created using ISO template that prepared in above steps.
+- Reference ISO will be unmounted.
+
+Hence, there are two main parts of this project:
+
+- Script itself and template files as essentials (**bootstrap.sh** and templ\_\* files explained in Footnotes section)
+- An additional package list to install during OS installation (**packages.txt**).
+
+### Requirements
+
+- CentOS 8
+
+  You can run script on CentOS 8 only (since it depends on CentOS 8 utilities).
+    
+- Some additional packages needs to be installed in order to run the script. Those can be installed by using the command below:
+
+        yum -y install yum-utils createrepo syslinux genisoimage isomd5sum bzip2 curl
+
+- One of the official ISO image of CentOS 8 distribution. Place it to same folder with the script.
+
+        CentOS-8-x86_64-XXXX-boot.iso
+        CentOS-8-x86_64-XXXX-dvd1.iso
+
+### Synopsis
+
+Basic usage of the script:
+
+    # ./bootstrap.sh 
+    Usage: ./bootstrap.sh <run [force] | clean | debug [package [package ..]] | step ..>
+
+Alternative usage for each step (in same workflow order) and usage for some functions:
+
+    # ./bootstrap.sh step
+    Usage: ./bootstrap.sh step ..
+
+    Workflow steps:
+        isomount
+        createtemplate
+        scandeps
+        createrepo
+        createiso
+        isounmount
+
+    Some usefull functions:
+        rpmname <package> [package ..]
+        rpmurl <package> [package ..]
+        rpmdownload <package> [package ..]
+        fulldeps <package>
+
+### Usage
+
+You can change the content of "packages.txt" if you wish and then simply run following command:
+
+    # ./bootstrap.sh run
+
+Script will continue to use the ISO template resource (image/) created at the first run on consequent runs. If you did changes on anywhere, you should "force" it to start job from the scratch by issuing following command;
+
+    # ./bootstrap.sh run force
+    
+Above command is actually equal to following two commands:
+
+    # ./bootstrap.sh clean
+    # ./bootstrap.sh run
+
+The "clean" switch will do "isounmount" when necessary and will clean ISO template and temporary files created by the script.
+
+You can run particular workflow-step manually when needed. For example:
+
+    # ./bootstrap.sh step createiso
+    
+There are also some functions which will not do any effect on working data. 
+
+For examople you can get download links for given package(s):
+
+     # ./bootstrap.sh step rpmurl httpd php 
+ 
+Or, you can download RPM files directly by issuing;
+
+    # ./bootstrap.sh step rpmdownload httpd php
+
+Dowloaded RPM files will be placed into "rpms/" folder for later use (will not be added into ISO template).
+
+If you want to add custom package(s) into the resulting ISO and you want it to be installed during OS installation, you can add package name(s) into **packages.txt** file. For example;
+
+**packages.txt**:
+
+    # Some networking tools
+    net-tools
+    ethtool
+    tcpdump
+
+    # Some system tools
+    pciutils
+
+    # Apache - PHP related
+    httpd
+    mod_security
+    mod_ssl
+    php
+    php-mbstring
+    php-soap
+
+(Empty lines and lines starting with "#" will be ignored.)
+
+If you want to download package(s) together with it's full dependencies you can use "debug" main switch. This switch is also useful to track processing of given package(s) if something went wrong.
+
+    # ./bootstrap.sh debug php
+
+This "debug" switch will create verbose output.
+Again, dowloaded RPM files will be placed into "rpms/" folder for later use (and will not be added into ISO template).
+
+### Environent Variables
+
+- **CMVERBOSE**=\<any value\>
+
+   It is possible to have verbose output by using this variable with any value. For example;
+   
+        # CMVERBOSE=1 ./bootstrap.sh run force
+
+   It is not set by default (no verbose output).
+         
+- **CMISO**="referece iso filename"
+
+   You can specify the reference ISO to be used with this variable like;
+   
+        # CMISO="CentOS-8-x86_64-1905-boot.iso" ./bootstrap.sh run force
+
+   Script will use "CentOS-8-x86\_64-1905-boot.iso" by default if such variable is not given.
+- **CMOUT**="resulting iso filename"
+
+   You can specify the name of resulting ISO file. For example;
+   
+        # CMOUT="my-minimal-centos-8.iso" ./bootstrap.sh run force    
+- **CMETH**=\<**fast** \| **deep**\>
+
+   The "method" that will be used while resolving package dependencies. You can combine it with *CMVERBOSE* for debugging purposes.
+
+   The script will use "fast" method by default. This will use system utilities to resolve dependencies (by issuing "repoquery --requires --resolve --recursive <package>") and will use what it returns as the list of packages.
+
+   The "deep" method is a kind of custom implementation of dependency resolving process. It is really slow since it checks each package recursively with its dependencies. But, it will give an idea about the dependency resolving process and it may also useful for debugging if something went wrong.
+   
+   On the other hand, you can use "deep" method with "debug" switch together to do debugging on single package. For example;
+   
+        # CMETH="deep" ./bootstrap.sh debug php
+    
+   Above command will display each package checks recursively and will display a dependency tree when it finished resolving.
+
+### Footnotes
+
+- Group file template
+
+   The list of "core" packages which defined in "templ\_comps.xml" template file is obtained from the "BaseOS" group of official CentOS 8 DVD-ISO.
+   
+   First, tt's content will be used to collect required installation packages. Then, the list of packages specified in "package.txt" will be merged into it and will be used as group file to include in metadata of the resulting ISO.
+   
+   You can change such file if you want, but please keep a single empty line inside the "packagelist" of group "core" to merge additional packages defined in package.txt like;
+
+   ```
+   <group>
+      <id>core</id>
+      <name>Core</name>
+      <packagelist>
+         <packagereq type="optional">tboot</packagereq>
+--- put single empty line here. ---
+      </packagelist>
+   </group>
+   ```
+   
+- Other Template files
+
+   The template file "tepl\_treeinfo" is obtained from official CentOS 8 DVD-ISO (".treeinfo" is the original name) . It's content will be re-constructed according to the reference ISO you are going to use and the resulting file will be added into ISO template.
+   
+   Other template files (templ\_discinfo, templ\_media.repo) are obtained from official CentOS 8 DVD-ISO and they will be added into resulting ISO as it is.
+   
+   
---
+++ bootstrap.sh
@@ -0,0 +1,653 @@
+#!/bin/bash
+# 2019'10, gokhan@kylone.com
+
+#
+# Evironment variables
+#
+#    CMVERBOSE  : increase verbosity (set any value)
+#    CMISO      : official ISO to use (CentOS-8-x86_64-1905-boot.iso)
+#    CMOUT      : resultig ISO file name (centos-8-minimal.iso)
+#    CMETH      : dependency resolving method to use (deep or fast)
+#
+# Default values
+#
+# official ISO to use
+iso="CentOS-8-x86_64-1905-boot.iso"
+#
+# resulting ISO file name
+out="centos-8-minimal.iso"
+#
+# dependency resolving method
+# deep: check dependency of every package one by one
+# fast: check core package depedencies only
+met="fast"
+#
+# no need to change further
+
+pw="$(pwd)"
+dp="${pw}/image"
+md="${pw}/mtemp"
+bo="${dp}/BaseOS"
+
+function cmusage() {
+   echo "Usage: ${0} <run [force] | clean | debug [package [package ..]] | step ..>"
+   echo
+   exit 1
+}
+
+function cmusagestep() {
+   echo "Usage: ${0} step .."
+   echo
+   echo " Workflow steps:"
+   echo "    isomount"
+   echo "    createtemplate"
+   echo "    scandeps"
+   echo "    createrepo"
+   echo "    createiso"
+   echo "    isounmount"
+   echo
+   echo " Some usefull functions:"
+   echo "    rpmname <package> [package ..]"
+   echo "    rpmurl <package> [package ..]"
+   echo "    rpmdownload <package> [package ..]"
+   echo "    fulldeps <package> [package ..]"
+   echo
+   exit 1
+}
+
+function cmnotcentos() {
+   echo
+   echo " ! This script is not suitable to use in this platform"
+   echo
+   exit 1
+}
+
+function cmcheck() {
+  if [ "${PIPESTATUS[0]}" != "0" ]; then
+    exit 1
+  fi
+}
+
+function cmpipe() {
+   while read line; do
+      echo "   ${1}${line}"
+   done
+}
+
+function cmdot() {
+   if [ "${CMVERBOSE}" != "" ]; then
+      cmpipe
+   else
+      echo -n "   "
+      while read line; do
+         echo -n "."
+      done
+      echo " done"
+   fi
+}
+
+function cmisounmount() {
+   if [ -d "${md}" ]; then
+      echo -n " ~ unmount ISO .."
+      umount "${md}" 2>/dev/null
+      rmdir "${md}"
+      echo " done"
+   fi
+}
+
+function cmisomount() {
+   if [ ! -e "${iso}" ]; then
+      echo
+      echo " ! CentoOS 8 (1905) NetInstall x86_64 ISO not found."
+      echo
+      echo "   You can download it from following resource;"
+      echo "   http://mirror.centos.org/centos/8.0.1905/isos/x86_64/"
+      echo
+      echo "   Once you downloaded, place file here with its original name:"
+      echo "   ${iso}"
+      echo
+      exit 1
+   fi
+   cmisounmount
+   echo " ~ mount ISO "
+   if [ ! -d "${md}" ]; then
+      mkdir -p "${md}"
+      mount -o loop "${iso}" "${md}" 2>&1 | cmpipe
+      cmcheck
+      echo "   ${md} mounted"
+   fi
+}
+
+function cmclean() {
+   cmisounmount
+   rm -rf "${dp}"
+   rm -f target_comps.xml "${out}" .[cpmrdtf]*
+}
+
+function cmcreatetemplate() {
+   if [ ! -d "${md}" ]; then
+      if [ "${CMSTEP}" != "" ]; then
+         echo " ! ISO not mounted, please run;"
+         echo "   ${0} step isomount"
+         echo
+      fi
+      return
+   fi
+   echo -n " ~ Preparing image template "
+   echo -n "."
+   mkdir -p "${dp}"
+   mkdir -p "${bo}/Packages"
+   echo -n "."
+   cp -r "${md}/EFI" "${dp}/"
+   cmcheck
+   echo -n "."
+   cp "templ_discinfo" "${dp}/.discinfo"
+   cp "templ_media.repo" "${dp}/media.repo"
+   echo -n "."
+   cp -r "${md}/isolinux" "${dp}/"
+   cmcheck
+   echo -n "."
+   cp -r "${md}/images" "${dp}/"
+   cmcheck
+   rm -f "${dp}/.treeinfo"
+   touch "${dp}/.treeinfo"
+   while IFS=  read line; do
+      imgf="$(echo "${line}" | grep "^images/" | awk -F" = " {'print $1'})"
+      if [ "${imgf}" != "" ]; then
+         if [ ! -e "${dp}/${imgf}" ]; then
+            echo
+            echo
+            echo " ! Image '${imgf}' not found in base ISO"
+            echo
+            exit 1
+         fi
+         sum="$(sha256sum "${dp}/${imgf}" | awk {'print $1'})"
+         echo "${imgf} = sha256:${sum}" >> "${dp}/.treeinfo"
+         echo -n "."
+      else
+         echo "${line}" >> "${dp}/.treeinfo"
+      fi
+   done < "templ_treeinfo"
+   echo " done"
+}
+
+function resolvefast() {
+   # input arguments
+   # package [package ..]
+   tf="${CMTEMP}"
+   vb="${CMVERBOSE}"
+   if [ "${vb}" != "" ]; then
+      echo "${@}" | tr " " "\n" | cmpipe "   "
+   fi
+   repoquery --requires --resolve --recursive "${@}" 2>/dev/null | \
+      awk -F":" {'print $1'} | \
+      sed 's/\-[0-9]\+$//g' | \
+      sort | uniq | \
+      grep -v "glibc-all-langpacks\|glibc-langpack-[a-z0-9]\+$" \
+   >> "${tf}"
+}
+
+function resolvedeep() {
+   # input arguments
+   # package seperator tempfile verbose
+   s="${CMSEP}-"
+   tf="${CMTEMP}"
+   vb="${CMVERBOSE}"
+   repoquery --requires --resolve "${@}" 2>/dev/null | \
+      awk -F":" {'print $1'} | \
+      sed 's/\-[0-9]\+$//g' | \
+      sort | uniq | \
+   while read line; do
+      if [ "${line}" == "glibc-all-langpacks" -o "$(echo "${line}" | grep "glibc-langpack-[a-z0-9]\+$")" != "" ]; then
+         if [ "${vb}" != "" ]; then
+            echo "       skip: ${@}	${line}"
+         fi
+         continue
+      fi
+      if [ "$(cat "${tf}" | grep "^${line}$")" == "" ]; then
+         echo "${s} ${line}" >> .tree
+         echo "${line}" >> "${tf}"
+         if [ "${vb}" != "" ]; then
+            echo "    package: ${@}	${line}"
+         else
+            echo -n ","
+         fi
+         CMSEP="${s}" resolvedeep "${line}"
+      fi
+   done
+}
+
+function cmfulldeps() {
+   # input arguments
+   # package [package ..]
+   if [ "${1}" == "" ]; then
+      echo "Usage: ${0} step fulldeps <package> [package ..]"
+      echo
+      exit
+   fi
+   rm -f ".pkgs" ".tree"
+   touch ".pkgs" ".tree"
+   echo " ~ Resolving dependencies for ${@}"
+   if [ "${met}" == "deep" ]; then
+      CMVERBOSE=1 CSEP=" " CMTEMP=".pkgs" resolvedeep "${@}"
+   else
+      rm -f .fast
+      CMVERBOSE=1 CMTEMP=".fast" resolvefast "${@}"
+      cat .fast | sort | uniq > .pkgs
+      rm -f .fast
+   fi
+   echo " ~ Full dependency list of ${1}"
+   cat .pkgs | sort | cmpipe "   "
+}
+
+function cmcreatelist() {
+   echo -n " ~ Creating package list "
+   rm -f .core
+   echo -n "."
+   cat templ_comps.xml | grep packagereq | awk -F">" {'print $2'} | awk -F"<" {'print $1'} > .core
+   echo -n "."
+   cat packages.txt | grep -v "^#" | grep -v "^$" >> .core
+   echo " done"
+   tp="$(cat .core | sort | uniq | wc -l)"
+   echo " ~ Resolving dependencies for ${tp} package(s)"
+   if [ "${CMVERBOSE}" == "" ]; then
+      echo -n "   "
+   fi
+   rm -f .tree .pkgs
+   touch .pkgs
+   cat .core | sort | uniq | while read line; do
+      if [ "${met}" == "deep" ]; then
+         if [ "${CMVERBOSE}" != "" ]; then
+            CMTEMP=".pkgs" CMSEP=" " resolvedeep "${line}"
+         fi
+      fi
+      echo "${line}" >> .pkgs
+      if [ "${CMVERBOSE}" == "" ]; then
+         echo -n "."
+      fi
+   done
+   if [ "${met}" == "deep" ]; then
+      if [ "${CMVERBOSE}" == "" ]; then
+         CMTEMP=".pkgs" CMSEP=" " resolvedeep $(cat .core | sort | uniq | tr "\n" " ")
+      fi
+   else
+      CMTEMP=".pkgs" resolvefast $(cat .core | sort | uniq | tr "\n" " ")
+   fi
+   rm -f .core
+   cat .pkgs | sort | uniq > .pkgf
+   mv .pkgf .pkgs
+   if [ "${CMVERBOSE}" == "" ]; then
+      echo " done"
+   fi
+}
+
+function cmrpmdownload() {
+   # input arguments
+   # package [package ..]
+   if [ "${1}" == "" ]; then
+      echo "Usage: ${0} rpmdownload <package>"
+      echo 
+      exit 1
+   fi
+   yumdownloader --urls "${@}" 2>/dev/null | \
+      grep "^http" | \
+      sort | uniq | \
+   while read u; do
+      if [ "${u}" != "" ]; then
+         f=`echo "${u}" | awk -F"/" {'print $NF'}`
+         if [ -e "rpms/${f}" ]; then
+            if [ "$(file "rpms/${f}" | grep "RPM ")" != "" ]; then
+               echo " - exists (rpms/${f})"
+               continue
+            fi
+            rm -f "rpms/${f}"
+         fi
+         echo "   ${f} [${u}]"
+         curl -s "${u}" -o "rpms/${f}"
+         if [ "${?}" == "0" ]; then
+            if [ "$(file "rpms/${f}" | grep "RPM ")" == "" ]; then
+               rm -f "rpms/${f}"
+               echo " ! failed"
+            fi
+         else
+            rm -f "rpms/${f}"
+            echo " ! failed"
+         fi
+      fi
+   done
+}
+
+function rpmdownload() {
+   # input arguments
+   # package [package ..]
+   if [ "${1}" == "" ]; then
+      echo " ! Pacakge name required for rpmdownload"
+      echo 
+      exit 1
+   fi
+   ul="${CMURL}"
+   if [ "${ul}" == "" ]; then
+      ul="$(yumdownloader --urls "${@}" 2>/dev/null | \
+            grep "^http" | \
+            sort | uniq)"
+   fi
+   echo "${ul}" | while read u; do
+      if [ "${u}" != "" ]; then
+         f=`echo "${u}" | awk -F"/" {'print $NF'}`
+         if [ -e "rpms/${f}" ]; then
+            if [ "$(file "rpms/${f}" | grep "RPM ")" != "" ]; then
+               echo "${f}"
+               continue
+            fi
+            rm -f "rpms/${f}"
+         fi
+         if [ -e "cache/${f}" ]; then
+            cp "cache/${f}" "rpms/"
+            echo "${f}"
+            continue
+         fi
+         curl -s "${u}" -o "rpms/${f}"
+         if [ "${?}" == "0" ]; then
+            if [ "$(file "rpms/${f}" | grep "RPM ")" != "" ]; then
+               echo "${u}" >> .dlrpm
+               echo "${f}"
+            else
+               rm -f "rpms/${f}"
+               echo "${u} -> ${f}" > .dler
+            fi
+         else
+            rm -f "rpms/${f}"
+            echo "${u} -> ${f}" > .dler
+         fi
+      fi
+   done
+}
+
+function cmrpmurl() {
+   # input arguments
+   # package [package ..]
+   if [ "${CMSTEP}" != "" -a "${1}" == "" ]; then
+      echo "Usage: ${0} step rpmurl <package> [package ..]"
+      echo 
+      exit 1
+   fi
+   yumdownloader --urls "${@}" 2>/dev/null | \
+      grep "^http" | \
+      sort | uniq
+}
+
+function cmrpmname() {
+   # input arguments
+   # package [package ..]
+   if [ "${CMSTEP}" != "" -a "${1}" == "" ]; then
+      echo "Usage: ${0} step rpmname <package> [package ..]"
+      echo 
+      exit 1
+   fi
+   repoquery "${@}" 2>/dev/null | \
+      sed 's/\-[0-9]\+:/\-/g' | \
+      awk {'print $1".rpm"'} | \
+      sort | uniq
+}
+
+function cmcollectrpm() {
+   # input arguments
+   # package [package ..]
+   vb="${CMVERBOSE}"
+   if [ "${CMSTEP}" != "" -a "${1}" == "" ]; then
+      echo "Usage: ${0} step collectrpm <package> [package ..]"
+      echo 
+      exit 1
+   fi
+   dl="$(cmrpmurl "${@}")"
+   rr="$(echo "${dl}" | awk -F"/" {'print $NF'} | sed 's/\.i686/\.x86_64/g' | sort | uniq)"
+   if [ "${rr}" != "" ]; then
+      echo "${rr}" | while read r; do
+         if [ -e "rpms/${r}" ]; then
+            if [ -d "${bo}/Packages" ]; then
+               cp "rpms/${r}" "${bo}/Packages/"
+            fi
+            if [ "${vb}" != "" ]; then
+               echo "     cached: ${r}"
+            else
+               echo -n "."
+            fi
+         else
+            pk="$(echo "${r}" | awk -F".el8" {'print $1'} | sed 's/\-[0-9\.\-]\+$//g')"
+            fu="$(echo "${dl}" | grep "/${r}$")"
+            if [ "${fu}" == "" ]; then
+               ir="$(echo "${r}" | sed 's/\.x86_64/\.i686/g')"
+               fu="$(echo "${dl}" | grep "/${ir}$")"
+            fi
+            if [ "$(echo "${fu}" | wc -l)" != "1" ]; then
+               fu=""
+               rp="$(echo "${r}" | awk -F".rpm" {'print $1'})"
+               pk="$(dnf info "${rp}" | grep "^Name" | awk -F": " {'print $2'} | sort | uniq)"
+            fi
+            if [ "${vb}" != "" ]; then
+               echo "downloading: ${pk}, ${r}"
+            fi
+            dd="$(CMURL="${fu}" rpmdownload "${pk}")"
+            if [ "${dd}" == "" ]; then
+               echo "${pk}:${r}:<none>" >> .miss
+               if [ "${vb}" != "" ]; then
+                  echo "  not found: ${r} (${pk})"
+               else
+                  echo -n "!"
+               fi
+            else
+               echo "${dd}" | while read d; do
+                  if [ "${d}" != "${r}" ]; then
+                     if [ -d "${bo}/Packages" ]; then
+                        cp "rpms/${d}" "${bo}/Packages/"
+                     fi
+                     if [ "${vb}" != "" ]; then
+                        echo " dowmloaded: ${r} -> ${d}, ${pk}"
+                     else
+                        echo -n ":"
+                     fi
+                  else
+                     if [ -d "${bo}/Packages" ]; then
+                        cp "rpms/${d}" "${bo}/Packages/"
+                     fi
+                     if [ "${vb}" == "" ]; then
+                        echo -n ":"
+                     fi
+                  fi
+               done
+            fi
+         fi
+      done
+   else
+      echo "${@}" >> .rslv
+      if [ "${@}" != "" ]; then
+         echo " unresolved: ${@}"
+      else
+         echo -n "!"
+      fi
+   fi
+}
+
+function cmcollectrpms() {
+   tp="$(cat .pkgs | sort | uniq | wc -l)"
+   echo " ~ Searching RPMs for ${tp} package(s)"
+   if [ "${CMVERBOSE}" == "" ]; then
+      echo -n "   "
+   fi
+   rm -f .miss .rslv .dler
+   mkdir -p rpms
+   cmcollectrpm $(cat .pkgs | sort | uniq | tr "\n" " ")
+   if [ "${CMVERBOSE}" == "" ]; then
+      echo " done"
+   fi
+}
+
+function cmcreaterepo() {
+   if [ ! -d "${bo}/Packages" ]; then
+      echo " ! Image temmplate is not ready, please run;"
+      echo "   ${0} step createtemplate"
+      echo "   ${0} step scandeps"
+      echo
+      exit 1
+   fi
+   tp="$(cat "${pw}/packages.txt" | grep -v "^#" | grep -v "^$" | wc -l)"
+   echo -n " ~ Creating component list for ${tp} add-on package "
+   uc="${pw}/target_comps.xml"
+   rm -f "${uc}"
+   touch "${uc}"
+   while IFS=  read xl; do
+      if [ "${xl}" == "" ]; then
+         cat "${pw}/packages.txt" | grep -v "^#" | grep -v "^$" | while read line; do
+            echo "      <packagereq type=\"default\">${line}</packagereq>" >> "${uc}"
+            echo -n "."
+         done
+      else
+         echo "${xl}" >> "${uc}"
+      fi
+   done < "${pw}/templ_comps.xml"
+   echo " done"
+   echo " ~ Creating repodata "
+   cd "${bo}"
+   cmcheck
+   rm -rf repodata
+   createrepo -g "${uc}" . 2>&1 | cmdot
+   cmcheck
+   cd "${pw}"
+   rm -f "${uc}"
+}
+
+function cmcreateiso() {
+   if [ ! -d "${bo}/repodata" ]; then
+      echo " ! Repo is not ready, please run;"
+      echo "   ${0} step createrepo"
+      echo
+      exit 1
+   fi
+   echo " ~ Creating ISO image"
+   cd "${dp}"
+   chmod 664 isolinux/isolinux.bin
+   rm -f "${pw}/${out}"
+   mkisofs \
+      -input-charset utf-8 \
+      -o "${pw}/${out}" \
+      -b isolinux/isolinux.bin \
+      -c isolinux/boot.cat \
+      -no-emul-boot \
+      -V 'CentOS-8-BaseOS-x86_64' \
+      -boot-load-size 4 \
+      -boot-info-table \
+      -R -J -v -T . 2>&1 | cmdot
+      cmcheck
+   if [ -e "/usr/bin/isohybrid" ]; then
+      echo " ~ ISO hybrid"
+      isohybrid "${pw}/${out}" | cmdot
+      cmcheck
+   fi
+   if [ -e "/usr/bin/implantisomd5" ]; then
+      echo " ~ Implant ISO MD5"
+      implantisomd5 --force --supported-iso "${pw}/${out}" | cmdot
+      cmcheck
+   fi
+   cd "${pw}"
+   isz="$(du -h "${out}" | awk {'print $1'})"
+   echo " ~ ISO image ready: ${out} (${isz})"
+}
+
+function cmjobsingle() {
+   # input arguments
+   # package [package ..]
+   rm -f .[cpmrdtf]*
+   touch .pkgs .tree
+   echo " ~ Creating package list for ${@} "
+   if [ "${met}" == "deep" ]; then
+      CMVERBOSE=1 CMTEMP=".pkgs" CMSEP=" " resolvedeep "${@}"
+   else
+      rm -f .fast
+      CMVERBOSE=1 CMTEMP=".fast" resolvefast "${@}"
+      cat .fast | sort | uniq > .pkgs
+   fi
+   echo " ~ Package with dependencies"
+   cat .pkgs | cmpipe "   "
+   if [ "${met}" == "deep" ]; then
+      echo " ~ Dependency tree"
+      cat .tree | cmpipe "   "
+   fi
+   echo " ~ Searching RPMs"
+   CMVERBOSE=1 cmcollectrpm $(cat .pkgs | sort | uniq | tr "\n" " ")
+}
+
+function cmscandeps() {
+   cmcreatelist
+   cmcollectrpms
+}
+
+function cmjobfull() {
+   cmclean
+   cmisomount
+   cmcreatetemplate
+   cmscandeps
+   cmcreaterepo
+   cmcreateiso
+   cmisounmount
+}
+
+function cmjobquick() {
+   cmcreatetemplate
+   cmcreaterepo
+   cmcreateiso
+}
+
+if [ ! -e /etc/centos-release ]; then
+   cmnotcentos
+fi
+if [ "$(cat /etc/centos-release | grep "CentOS Linux release 8.0")" == "" ]; then
+   cmnotcentos
+fi
+if [ ! -e "/usr/bin/repoquery" -o ! -e "/usr/bin/createrepo" -o ! -e "/usr/bin/yumdownloader" -o ! -e "/usr/bin/curl" -o ! -e "/usr/bin/mkisofs" ]; then
+   echo
+   echo " ! Some additional packages needs to be installed."
+   echo "   Please run following command to have them all:"
+   echo
+   echo "   yum -y install yum-utils createrepo syslinux genisoimage isomd5sum bzip2 curl"
+   echo
+   exit 1
+fi
+if [ "${CMISO}" != "" ]; then
+   iso="${CMISO}"
+fi
+if [ "${CMOUT}" != "" ]; then
+   out="${CMOUT}"
+fi
+if [ "${CMETH}" != "" ]; then
+   met="${CMETH}"
+fi
+
+if [ "${1}" == "run" ]; then
+   shift
+   if [ "${1}" == "force" ]; then
+      cmjobfull
+   elif [ -d "${bo}/Packages" ]; then
+      cmjobquick
+   else
+      cmjobfull
+   fi
+elif [ "${1}" == "clean" ]; then
+   cmclean
+elif [ "${1}" == "debug" ]; then
+   shift
+   if [ "${1}" == "" ]; then
+      cmusage
+   fi
+   cmjobsingle "${@}"
+elif [ "${1}" == "step" ]; then
+   shift
+   if [ "${1}" == "" ]; then
+      cmusagestep
+   fi
+   cmd="cm${1}"
+   shift
+   CMVERBOSE=1 CMSTEP=1 ${cmd} "${@}"
+else
+   cmusage
+fi
+
